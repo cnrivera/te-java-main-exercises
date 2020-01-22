0118cf0f2fe8938c5fa15c1865f9c9c78454afc9
David P-C <david.pc@techelevator.com>
2020-01-22 08:51:40 -0500

    Added student-lecture for m1-08.

---
+++ module-1/08_Collections_Part_2/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,53 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+
+		System.out.println("####################");
+		System.out.println("       SETS");
+		System.out.println("####################");
+		System.out.println();
+		
+
+		System.out.println("####################");
+		System.out.println("Sets cannot contain duplicates");
+		System.out.println("####################");
+		System.out.println();
+
+
+		System.out.println("####################");
+		System.out.println("Sets do not guarantee ordering");
+		System.out.println("####################");
+		System.out.println();
+
+
+		System.out.println("####################");
+		System.out.println("       MAPS");
+		System.out.println("####################");
+		System.out.println();
+		
+		/* Map is an interface and HashMap is a class that implements Map */
+
+		/* The "put" method is used to add elements to a Map */
+
+		/* The "get" method is used to retrieve elements from a Map */
+	
+		/* keySet returns a Set of all of the keys in the Map */
+		
+		/* If the key already exists, put will overwrite the existing value with the new value */
+	}
+
+}
