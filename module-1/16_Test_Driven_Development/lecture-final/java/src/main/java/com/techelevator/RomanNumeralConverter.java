6a4476f310816139ccb90db180ae30be69d11703
David P-C <david.pc@techelevator.com>
2020-01-31 10:36:33 -0500

    Refactored convertToRoman.

--- module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
+++ module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
@@ -3,11 +3,9 @@ package com.techelevator;
 public class RomanNumeralConverter {
 
 	public String convertToRoman(int input) {
-		String result = "II";
-		if (input == 1) {
-			result = "I";
-		} else if (input == 3) {
-			result = "III";
+		String result = "";
+		for (int i = 0; i < input; i++) {
+			result += "I";
 		}
 		return result;
 	}
