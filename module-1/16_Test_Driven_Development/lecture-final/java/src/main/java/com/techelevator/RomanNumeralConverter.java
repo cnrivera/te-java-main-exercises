8b2159d217a3d83ac03b98355b89df89e9bbed75
David P-C <david.pc@techelevator.com>
2020-01-31 11:37:15 -0500

    Added passing code for converting_multiples_of_4_returns_expected_values

--- module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
+++ module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
@@ -10,9 +10,11 @@ public class RomanNumeralConverter {
 		SYMBOLS.put(1000, "M");
 		SYMBOLS.put(900, "CM");
 		SYMBOLS.put(500, "D");
+		SYMBOLS.put(400, "CD");
 		SYMBOLS.put(100, "C");
 		SYMBOLS.put(90, "XC");
 		SYMBOLS.put(50, "L");
+		SYMBOLS.put(40, "XL");
 		SYMBOLS.put(10, "X");
 		SYMBOLS.put(9, "IX");
 		SYMBOLS.put(5, "V");
