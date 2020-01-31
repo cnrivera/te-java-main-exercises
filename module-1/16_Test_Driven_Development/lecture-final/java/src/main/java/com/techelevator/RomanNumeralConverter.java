992dcb492364023e18cc67bd02c27f6f0e7280bf
David P-C <david.pc@techelevator.com>
2020-01-31 11:10:41 -0500

    Refactored RomanNumeralConverter, adding doConversionStep method

--- module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
+++ module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
@@ -5,15 +5,19 @@ public class RomanNumeralConverter {
 	public String convertToRoman(int input) {
 		String result = "";
 
-		while (input >= 1000) {
-			result += "M";
-			input -= 1000;
-		}
+		result += doConversionStep(input, 1000, "M");
+		input = input % 1000;
+		result += doConversionStep(input, 1, "I");
 
-		if (input > 0) {
-			for (int i = 0; i < input; i++) {
-				result += "I";
-			}
+		
+		return result;
+	}
+	
+	private String doConversionStep(int input, int value, String symbol) {
+		String result = "";
+		while (input >= value) {
+			result += symbol;
+			input -= value;
 		}
 		return result;
 	}
