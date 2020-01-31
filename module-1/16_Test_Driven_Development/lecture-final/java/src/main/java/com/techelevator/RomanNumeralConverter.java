d12ec0e8f6278ceb8c562ba54cf1dc6dfa2b54b0
David P-C <david.pc@techelevator.com>
2020-01-31 11:02:14 -0500

    Added passing code for converting_1001_returns_MI

--- module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
+++ module-1/16_Test_Driven_Development/lecture-final/java/src/main/java/com/techelevator/RomanNumeralConverter.java
@@ -5,11 +5,12 @@ public class RomanNumeralConverter {
 	public String convertToRoman(int input) {
 		String result = "";
 
-		if (input >= 1000) {
-			for (int i = 0; i < input; i += 1000) {
-				result += "M";
-			}
-		} else {
+		while (input >= 1000) {
+			result += "M";
+			input -= 1000;
+		}
+
+		if (input > 0) {
 			for (int i = 0; i < input; i++) {
 				result += "I";
 			}
