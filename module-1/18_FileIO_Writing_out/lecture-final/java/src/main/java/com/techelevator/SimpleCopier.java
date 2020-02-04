0e9bdf070283f89ab569b14aca084050f2cf99a7
David P-C <david.pc@techelevator.com>
2020-02-04 14:14:20 -0500

    Cleaned up SimpleCopier in lecture-final for m1-18.

--- module-1/18_FileIO_Writing_out/lecture-final/java/src/main/java/com/techelevator/SimpleCopier.java
+++ module-1/18_FileIO_Writing_out/lecture-final/java/src/main/java/com/techelevator/SimpleCopier.java
@@ -9,45 +9,34 @@ import java.util.Scanner;
 public class SimpleCopier {
 
 	private static Scanner userInput = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
 		System.out.print("Enter the path of a file to copy: ");
 		String sourcePath = userInput.nextLine();
 		File sourceFile = new File(sourcePath);
-		
+
 		if (sourceFile.exists() && sourceFile.isFile()) {
 			System.out.print("Enter the path to copy to: ");
 			String destPath = userInput.nextLine();
 			File destFile = new File(destPath);
-			
+
 			if (destFile.exists() == false) {
 				try (Scanner sourceScanner = new Scanner(sourceFile)) {
-					if (destFile.createNewFile()) {
-						try (PrintWriter destWriter = new PrintWriter(destFile)) {
-							while (sourceScanner.hasNextLine()) {
-								String line = sourceScanner.nextLine();
-								destWriter.println(line);
-							}
+					try (PrintWriter destWriter = new PrintWriter(destFile)) {
+						while (sourceScanner.hasNextLine()) {
+							String line = sourceScanner.nextLine();
+							destWriter.println(line);
 						}
-						System.out.println("Copy finished.");
-					} else {
-						throw new IOException();
 					}
+					System.out.println("Copy finished.");
 				} catch (FileNotFoundException e) {
 					System.out.println("File was not found: " + e.getMessage());
-				} catch (IOException e) {
-					System.out.println("Couldn't create the new file!");
 				}
-				
 			} else {
 				System.out.println("That file already exists!");
 			}
-			
 		} else {
 			System.out.println("That's not an existing file!");
 		}
-
 	}
-
 }
