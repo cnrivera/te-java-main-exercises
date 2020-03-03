30f5d95caffaeb22952e21b93fc3ec012c7ab07f
David P-C <david.pc@techelevator.com>
2020-03-03 12:14:00 -0500

    Added lecture-final for m3-07.

---
+++ module-3/07-Forms-and-Controllers-HTTP-POST/lecture-final/java/src/main/java/com/techelevator/controller/SurveyController.java
@@ -0,0 +1,36 @@
+package com.techelevator.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.techelevator.model.Survey;
+import com.techelevator.model.SurveyDAO;
+
+@Controller
+public class SurveyController {
+	
+	@Autowired
+	private SurveyDAO surveyDao;
+
+	@RequestMapping(path="/surveyInput", method=RequestMethod.GET)
+	public String showSurveyInput() {
+		return "surveyInput";
+	}
+	
+	@RequestMapping(path="/surveyInput", method=RequestMethod.POST)
+	public String processSurveyInput(@ModelAttribute Survey currentSurvey) {
+		
+		surveyDao.save(currentSurvey);
+		
+		
+		return "redirect:/surveyResult";
+	}
+	
+	@RequestMapping(path="/surveyResult", method=RequestMethod.GET)
+	public String showSurveyResult() {
+		return "surveyResult";
+	}
+}
