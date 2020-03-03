9f09f2b90b3ef6f5a2d315af9e503d148e199cdb
David P-C <david.pc@techelevator.com>
2020-03-03 08:35:22 -0500

    Added student-exercise for m3-07.

---
+++ module-3/07-Forms-and-Controllers-HTTP-POST/student-exercise/java/src/main/java/com/techelevator/HelloController.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class HelloController {
+
+	@RequestMapping("/greeting")
+	public String displayGreeting() {		
+		return "greeting";
+	}
+}
