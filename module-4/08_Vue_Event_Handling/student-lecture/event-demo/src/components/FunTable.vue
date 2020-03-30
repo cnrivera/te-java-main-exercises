4b27a73058568fec2c0ef521a31bb3b00560ffe7
David P-C <david.pc@techelevator.com>
2020-03-30 08:48:19 -0400

    Added student-lecture for m4-08.

---
+++ module-4/08_Vue_Event_Handling/student-lecture/event-demo/src/components/FunTable.vue
@@ -0,0 +1,29 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+table {
+    table-layout: fixed;
+    border-collapse: collapse;
+    border: 1px solid black;
+    width: 100%;
+    font-size: 2em;
+}
+
+td {
+    border: 1px solid black;
+    height: 2em;
+}
+
+</style>
