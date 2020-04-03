be5f3e23c00811e248c3399b3f56c611d7263e13
David P-C <david.pc@techelevator.com>
2020-04-03 09:05:11 -0400

    Added student-lecture for m4-12 and te-snippets project.

---
+++ module-4/12_REST_APIs/student-lecture/product-reviews-api/src/test/java/com/techelevator/productreviewsapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techelevator.productreviewsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
